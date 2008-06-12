2a0a8e9c56ac5182ac495fed8c7145092cdc524d
Steve Landers <steve@digitalsmarties.com>
2008-06-12 08:59:46 +0000

    added C++ examples

---
+++ test/c++indirect.tcl
@@ -0,0 +1,20 @@
+package require critcl
+
+critcl::config language c++
+critcl::clibraries -lstdc++
+
+critcl::ccode {
+    class A {
+      int value;
+    public:
+      A() : value (123) {}
+      operator int() const { return value; }
+    };
+}
+
+critcl::cproc tryplus {} int {
+	A var;
+	return var;
+}
+
+puts "tryplus = [tryplus]"
