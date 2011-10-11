e5aeac49742f3700d4df30c37fb25d1efaff7725
Andreas Kupries <akupries@shaw.ca>
2011-10-11 09:19:15 -0700

    Fixed a bug exposed only on Windows. Thanks to Tom Krehbiel for bringing it to my attention.
    We have to handle the TCL_STORAGE_CLASS properly or the compiler and linker may choke. Interestingly the unfixed code was no problem for the MSVC6 I used, but MSYS/gcc barfed on it, either during compilation (mine), or linking (Tom's).

--- lib/critcl/critcl.tcl
+++ lib/critcl/critcl.tcl
@@ -1014,6 +1014,12 @@ proc ::critcl::API_setup_export {file} {
 	}
     }
 
+    # Insert code to handle the storage class settings on Windows.
+
+    append sdecls [string map \
+		       [list @cname@ $cname @up@ $upname] \
+		       $v::storageclass]
+
     package require stubs::container
     package require stubs::reader
     package require stubs::gen
@@ -1073,6 +1079,7 @@ proc ::critcl::API_setup_export {file} {
     }
 
     cinit $sinitrun $sinitstatic
+    cflags -DBUILD_$cname
 
     return $prefix
 }
@@ -3855,6 +3862,35 @@ namespace eval ::critcl {
 	    string  "  Tcl_SetResult(ip, rv, TCL_DYNAMIC);"
 	    dstring "  Tcl_SetResult(ip, rv, TCL_DYNAMIC);"
 	    vstring "  Tcl_SetResult(ip, rv, TCL_VOLATILE);"
+	}
+
+	variable storageclass {
+/*
+ * These macros are used to control whether functions are being declared for
+ * import or export. If a function is being declared while it is being built
+ * to be included in a shared library, then it should have the DLLEXPORT
+ * storage class. If is being declared for use by a module that is going to
+ * link against the shared library, then it should have the DLLIMPORT storage
+ * class. If the symbol is beind declared for a static build or for use from a
+ * stub library, then the storage class should be empty.
+ *
+ * The convention is that a macro called BUILD_xxxx, where xxxx is the name of
+ * a library we are building, is set on the compile line for sources that are
+ * to be placed in the library. When this macro is set, the storage class will
+ * be set to DLLEXPORT. At the end of the header file, the storage class will
+ * be reset to DLLIMPORT.
+ */
+
+#undef TCL_STORAGE_CLASS
+#ifdef BUILD_@cname@
+#   define TCL_STORAGE_CLASS DLLEXPORT
+#else
+#   ifdef USE_@up@_STUBS
+#      define TCL_STORAGE_CLASS
+#   else
+#      define TCL_STORAGE_CLASS DLLIMPORT
+#   endif
+#endif
 	}
 
 	variable code	         ;# This array collects all code snippets and
