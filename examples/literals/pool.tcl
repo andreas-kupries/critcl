1ba91f35ede26d8ba24c0b04d71a58dc6d30e589
Andreas Kupries <andreask@activestate.com>
2014-01-23 13:46:22 -0800

    Added example code/package for string/literal pool utility package.

---
+++ examples/literals/pool.tcl
@@ -0,0 +1,49 @@
+# pool.tcl --
+#
+#	A template demonstrating the handling of string/literal pools.
+#
+# Copyright (c) 2014 Andreas Kupries <andreas_kupries@users.sourceforge.net>
+
+# # ## ### ##### ######## ############# #####################
+## Requirements
+
+package require Tcl              8.4
+package require critcl           3.1.11
+package require critcl::literals 1
+
+# # ## ### ##### ######## ############# #####################
+## Administrivia
+
+critcl::license {Andreas Kupries} BSD
+
+critcl::summary {String pools}
+
+critcl::description {
+    This package implements nothing. It serves only as a
+    demonstration and template on how to declare a shared
+    string pool and use it in cproc's or ccommand's
+}
+
+critcl::subject demonstration {string pool} {literal pool} \
+    {shared strings} {shared literals}
+
+# # ## ### ##### ######## ############# #####################
+## C code.
+
+critcl::literals::def demo {
+    here  "here"
+    comes "comes"
+    the   "the"
+    sun   "sun"
+}
+
+critcl::cproc str {Tcl_Interp* ip int code} object {
+    return demo (ip, code);
+}
+
+# str 0
+# str 7 - panic, abort, core dump
+
+# ### ### ### ######### ######### #########
+## Ready
+package provide pool 1
