andreas-kupries/critcl

commit 86f415dd305fec7aa707b842def4368f5c2f4079

Fix interaction of mode "compile&run" with stubs handling. - stubs setup happens when the compilation phase is active. - Input commands consider this phase as "declared after build". - stubs setup used he regular input commands, wiped out the   collected state, then failed. - Fixed by splitting 3 input commands into core and user layer,   only the latter does the check for compilation phase - stubs setup now calls on the new core layer.
Found while working on the CRIMP demo app and having it build
the packages on the fly.

lib/critcl/critcl.tcl
@@ -148,6 +148,11 @@ proc ::critcl::Lines {text} {
 proc ::critcl::ccode {text} {
     set file [SkipIgnored [This]]
     HandleDeclAfterBuild
+    CCodeCore $file $text
+    return
+}
+
+proc ::critcl::CCodeCore {file text} {
     set digest [UUID.extend $file .ccode $text]
 
     set block {}
@@ -1032,7 +1037,7 @@ proc ::critcl::cconst {name rtype rvalue} {
     # constant return value, which allows the optimization of the
     # generated code. Only the shim is emitted, with the return value
     # in place. No need for a lower-level C function containing a
-    # funciton body.
+    # function body.
 
     SkipIgnored [set file [This]]
     HandleDeclAfterBuild
@@ -1157,7 +1162,11 @@ proc ::critcl::cproc {name adefs rtype {body "#"} args} {
 proc ::critcl::cinit {text edecls} {
     set file [SkipIgnored [set file [This]]]
     HandleDeclAfterBuild
+    CInitCore $file $text $edecls
+    return
+}
 
+proc ::critcl::CInitCore {file text edecls} {
     set digesta [UUID.extend $file .cinit.f $text]
     set digestb [UUID.extend $file .cinit.e $edecls]
 
@@ -1467,10 +1476,14 @@ proc ::critcl::cflags {args} {
     set file [SkipIgnored [This]]
     HandleDeclAfterBuild
     if {![llength $args]} return
+    CFlagsCore $file $args
+    return
+}
 
-    UUID.extend $file .cflags $args
+proc ::critcl::CFlagsCore {file flags} {
+    UUID.extend $file .cflags $flags
     dict update v::code($file) config c {
-	foreach flag $args {
+	foreach flag $flags {
 	    dict lappend c cflags $flag
 	}
     }
@@ -1999,10 +2012,10 @@ proc ::critcl::API_setup_import {file} {
 	    #include <$cname/${cname}Decls.h>
 	}]
 	append prefix \n$import
-	ccode $import
+	CCodeCore $file $import
 
 	# TODO :: DOCUMENT environment of the cinit code.
-	cinit [subst -nocommands {
+	CInitCore $file [subst -nocommands {
 	    if (!${capname}_InitStubs (ip, "$iversion", 0)) {
 		return TCL_ERROR;
 	    }
@@ -2043,7 +2056,7 @@ proc ::critcl::API_setup_export {file} {
 	#include <$cname/${cname}Decls.h>
     }]
     append prefix \n$import
-    ccode $import
+    CCodeCore $file $import
 
     # Generate the necessary header files.
 
@@ -2133,8 +2146,8 @@ proc ::critcl::API_setup_export {file} {
 	dict lappend r apiheader [file join [cache] $cname]
     }
 
-    cinit $sinitrun $sinitstatic
-    cflags -DBUILD_$cname
+    CInitCore  $file $sinitrun $sinitstatic
+    CFlagsCore $file [list -DBUILD_$cname]
 
     return $prefix
 }

commit 452ba39d2fccfabfc349c93e2c47af77edee6bc9

The separation of critcl::ccode into user and work layers means that location retrieval has to go one more level up to find the user location. Fixes bug introduced by commit 86f415dd30.

lib/critcl/critcl.tcl
@@ -157,7 +157,7 @@ proc ::critcl::CCodeCore {file text} {
 
     set block {}
     lassign [HeaderLines $text] leadoffset text
-    append block [at::CPragma $leadoffset -2 $file] $text \n
+    append block [at::CPragma $leadoffset -3 $file] $text \n
 
     dict update v::code($file) config c {
 	dict lappend c fragments $digest