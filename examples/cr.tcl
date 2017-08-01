011bf846fac1b68d7b9d82e9aa4c29aaec5de515
Andreas Kupries <akupries@shaw.ca>
2017-07-31 22:00:48 -0700

    Run examples in lexicographic order. Limit examples to run by glob matching.

--- examples/cr.tcl
+++ examples/cr.tcl
@@ -4,10 +4,14 @@
 set selfdir [file dirname [file normalize [info script]]]
 set self    [file tail [info script]]
 
+set pattern [lindex $argv 0]
+if {$pattern eq ""} {
+    set pattern *
+}
 # Perform "compile & run" in the sub-directories.
 
 puts ""
-foreach cr [glob -directory $selfdir */$self] {
+foreach cr [lsort -dict [glob -directory $selfdir $pattern/$self]] {
     puts "$cr _______________________________________________"
     catch {
 	eval [list exec 2>@ stderr >@ stdout [info nameofexecutable] $cr]
