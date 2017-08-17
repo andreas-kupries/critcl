f36f12ce89647843c07619df932f74626b524fc9
Andreas Kupries <akupries@shaw.ca>
2017-08-16 20:52:37 -0700

    Reworked the tracing API again. This looks ok now.

--- lib/critcl-cutil/trace/trace.c
+++ lib/critcl-cutil/trace/trace.c
@@ -11,64 +11,50 @@
 
 #ifdef CRITCL_TRACER
 
-/* Tracking the stack of functions,
+/* Tracking the stack of scopes,
  * single-linked list,
  * top to bottom.
  */
 
-typedef struct function_stack {
-    const char*            str;
-    struct function_stack* down;
-} function_stack;
+typedef struct scope_stack {
+    const char*         scope;
+    struct scope_stack* down;
+} scope_stack;
 
 /*
  * = = == === ===== ======== ============= =====================
- * Tracing state
+ * Tracing state (stack of scopes, associated indentation level)
  */
 
-static function_stack* top   = 0;
-static int             level = 0;
+static scope_stack* top   = 0;
+static int          level = 0;
 
 /*
  * = = == === ===== ======== ============= =====================
- * Internal - Function stack
+ * Internals
  */
 
 static void
-push (const char* str)
-{
-    function_stack* new = ALLOC (function_stack);
-    new->str = str;
-    new->down = top;
-    top = new;
-    level += 4;
-}
-
-static void
-pop (void)
-{
-    function_stack* next = top->down;
-    level -= 4;
-    ckfree ((char*)top);
-    top = next;
-}
-
-static void
-print_prefix (void)
+indent (void)
 {
     int i;
-    for (i = 0; i < level; i++) {
-	fwrite(" ", 1, 1, stdout);
-	fflush           (stdout);
-    }
+    for (i = 0; i < level; i++) { fwrite(" ", 1, 1, stdout); }
+    fflush (stdout);
+}
 
-    if (top) {
-	fwrite(top->str, 1, strlen(top->str), stdout);
-	fflush                               (stdout);
-    }
+static void
+scope (void)
+{
+    if (!top) return;
+    fwrite (top->scope, 1, strlen(top->scope), stdout);
+    fflush (stdout);
+}
 
-    fwrite(" ", 1, 1, stdout);
-    fflush           (stdout);
+static void
+separator (void)
+{
+    fwrite(" | ", 1, 3, stdout);
+    fflush             (stdout);
 }
 
 /*
@@ -77,84 +63,91 @@ print_prefix (void)
  */
 
 void
-critcl_trace_enter (int on, const char* fun, int nonewline)
+critcl_trace_push (const char* scope)
+{
+    scope_stack* new = ALLOC (scope_stack);
+    new->scope = scope;
+    new->down  = top;
+    top = new;
+    level += 4;
+}
+
+void
+critcl_trace_pop (void)
+{
+    scope_stack* next = top->down;
+    level -= 4;
+    ckfree ((char*)top);
+    top = next;
+}
+
+void
+critcl_trace_header (int on, int ind, const char* filename, int line)
 {
     if (!on) return;
-    push (fun);
-    print_prefix();
-    fwrite ("ENTER",                1, 5, stdout);
-    fwrite (nonewline ? " " : "\n", 1, 1, stdout);
+    // location prefix
+    if (filename) {
+	fprintf (stdout, "%s:%6d", filename, line);
+	fflush  (stdout);
+    }
+    // indentation, scope, separator
+    if (ind) { indent (); }
+    scope ();
+    separator();
+}
+
+void
+critcl_trace_closer (int on)
+{
+    if (!on) return;
+    fwrite ("\n", 1, 1, stdout);
     fflush (stdout);
 }
 
-/*
- * 1MB output-buffer. We may trace large data structures. This is also a
- * reason why the implementation can be compiled out entirely.
- */
-static char msg [1024*1024];
-
 void
-critcl_trace_return (int on, const char *pat, ...)
-{
+critcl_trace_printf (int on, const char *format, ...)
+{    
+    /*
+     * 1MB output-buffer. We may trace large data structures. This is also a
+     * reason why the implementation can be compiled out entirely.
+     */
+    static char msg [1024*1024];
     int len;
     va_list args;
-
     if (!on) return;
-    print_prefix();
-    fwrite("RETURN = ", 1, 9, stdout);
-    fflush                   (stdout);
-
-    va_start(args, pat);
-    len = vsprintf(msg, pat, args);
+    va_start(args, format);
+    len = vsprintf(msg, format, args);
     va_end(args);
-
-    msg[len++] = '\n';
-    msg[len] = '\0';
-
-    fwrite(msg, 1, len, stdout);
-    fflush             (stdout);
-
-    pop();
-}
-
-void
-critcl_trace_printf (int on, int indent, int nl, const char *pat, ...)
-{
-    int len;
-    va_list args;
-    static int closed;
-
-    if (!on) return;
-    if (closed && indent) print_prefix();
-
-    va_start(args, pat);
-    len = vsprintf(msg, pat, args);
-    va_end(args);
-    if (nl) {
-	msg[len++] = '\n';
-	msg[len] = '\0';
-	closed = 1;
-    } else {
-	closed = 0;
-    }
-
     fwrite(msg, 1, len, stdout);
     fflush             (stdout);
 }
 
 void
-critcl_trace_cmd_args (int argc, Tcl_Obj*const* argv)
+critcl_trace_cmd_args (const char* scopename, int argc, Tcl_Obj*const* argv)
 {
     int i;
+    critcl_trace_push (scopename);
     for (i=0; i < argc; i++) {
-	critcl_trace_printf (1, 1, 1, "ARG [%3d] = '%s'", i, Tcl_GetString((Tcl_Obj*) argv[i]));
+	// No location information
+	indent();
+	scope();
+	separator();
+	critcl_trace_printf (1, "ARG [%3d] = '%s'\n",
+			     i, Tcl_GetString((Tcl_Obj*) argv[i]));
     }
 }
 
-void
-critcl_trace_cmd_result (const Tcl_Obj* result)
+int
+critcl_trace_cmd_result (int status, Tcl_Interp* ip)
 {
-    critcl_trace_printf (1, 1, 1, "RESULT = '%s'", Tcl_GetString((Tcl_Obj*) result));
+    char* result = Tcl_GetString (Tcl_GetObjResult (ip));
+    // No location information
+    indent();
+    scope();
+    separator();
+    critcl_trace_printf (1, "RESULT = %d '%s'\n", status, result);
+    critcl_trace_pop ();
+    return status;
 }
 
 #endif /*  CRITCL_TRACER */
