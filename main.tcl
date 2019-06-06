054c661385a50e0c545722820d4f0d8ed91d27b9
sbrester <sergey.brester@dieboldnixdorf.com>
2019-06-06 12:25:44 +0200

    main.tcl: allow direct invoke (without starkit), so sourced/debug/dev-edition (uninstalled)

--- main.tcl
+++ main.tcl
@@ -1,5 +1,12 @@
-package require starkit
-if {[starkit::startup] == "sourced"} return
+if {![catch {
+  # kit related main:
+  package require starkit
+}]} {
+  if {[starkit::startup] == "sourced"} return
+} else {
+  # direct invoke without kit (sourced/debug/dev-edition):
+  lappend ::auto_path [file join [file dirname [info script]] lib]
+}
 package require critcl::app 3
 #puts [package ifneeded critcl [package require critcl::app 3]]
 critcl::app::main $argv
